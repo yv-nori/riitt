f06f5b0f9c063a28dec5d84bd987f5ecc023ac12
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-06-13 19:48:20 +0900

    webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,41 +1,11 @@
 const { environment } = require('@rails/webpacker')
-
-// jqueryを使える設定
-// const webpack = require('webpack')
-// environment.plugins.prepend('Provide',
-//   new webpack.ProvidePlugin({
-//     $: 'jquery/src/jquery',
-//     jQuery: 'jquery/src/jquery'
-//   })
-
-// )
-// jqueryを使える設定改
+// 以下を追記
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']
+    jQuery: 'jquery/src/jquery'
   })
 )
-// たぶんjquery-uiを使える設定
-environment.toWebpackConfig().merge({
-  resolve: {
-    alias: {
-      'jquery': 'jquery/src/jquery'
-    }
-  }
-});
-
-// sassを複数importする設定
-// $ yarn add import-glob-loaderをしてから
-const globCssImporter = require('node-sass-glob-importer');
-
-environment.loaders.get('sass')
-  .use
-  .find(item => item.loader === 'sass-loader')
-  .options
-  .sassOptions = { importer: globCssImporter() };
-
+// ここまで
 module.exports = environment
-node - sass - glob - importer
