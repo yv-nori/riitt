d3da14cc62517f6748248d3bbd30d3e017832346
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-06-13 18:58:08 +0900

    k

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,26 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-
-// Vue.js フル版（Compiler入り）
-environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
-
-// ここから
-// jQueryとBootstapのJSを使えるように
+const environment = require('./environment')
 const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: 'popper.js'
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
   })
 )
-// ここまで
-
 module.exports = environment.toWebpackConfig()
