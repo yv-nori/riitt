741c6e606e2da2540daaf16d5dc4c43e1375ee9f
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-06-13 17:15:20 +0900

    デプロイエラー jquery　おかしいので修正中

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,24 @@
 const { environment } = require('@rails/webpacker')
-// jqueryを使える設定改
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// ここから
+// jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
+environment.plugins.prepend(
+  'Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
   })
 )
+// ここまで
+
 module.exports = environment
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,11 +1,26 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const environment = require('./environment')
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// ここから
+// jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
+environment.plugins.prepend(
+  'Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
   })
 )
+// ここまで
+
 module.exports = environment.toWebpackConfig()
